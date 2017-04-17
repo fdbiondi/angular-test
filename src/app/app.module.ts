814f4c5a463246fbcbed8ef81e2dc0a721131e93
fdbiondi <fdbion@gmail.com>
2017-04-17 20:45:15 -0300

    test application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { ErrorAlertComponent } from './error-alert/error-alert.component';
+import { UsernameComponent } from './username/username.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent,
+    ErrorAlertComponent,
+    UsernameComponent
   ],
   imports: [
     BrowserModule,
